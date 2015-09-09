ed2aa44cb746ae804af2ee791650660885a7d760
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2015-09-09 01:37:53 -0500

    routing for users and guests, edited 404

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
+class AuthConstraint
+  def matches?(request)
+    request.session['user_id'].present?
+  end
+end
+
 HearkenFinal::Application.routes.draw do
-  resources :cats
+  constraints(AuthConstraint.new) do
+    resources :cats
+  end
 
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   
   root to: "home#index"
+
+  get "*any", to: redirect('/404')
 end
