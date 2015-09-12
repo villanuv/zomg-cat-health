1152c68bdd1d7bbb3fd4d73406e7c657badc1c1d
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2015-09-12 02:45:18 -0500

    cat test passes

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Cat, type: :model do
 
   it { should validate_presence_of :url }
   it { should validate_presence_of :description }
-  it { should belong_to(:user) }
+  it { should belong_to :user }
+  it { should have_many :votes }
 
 end
