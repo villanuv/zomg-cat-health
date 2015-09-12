36953b41ca2a0b72603345cdf842192767596cc3
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2015-09-12 03:29:27 -0500

    vote model tests pass

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ActiveRecord::Base
+  # validations
+  validates :option, presence: true
+  validates :ip_address, presence: true
+
   # associations
   belongs_to :cat
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of :option }
+  it { should validate_presence_of :ip_address }
+  it { should belong_to :cat }
+
 end
