98e600dd3081ce22d4733a3746f89bff0f8e4b7f
Patrick Villanueva <Patrick@Patricks-Mac-Pro.local>
2015-09-19 16:53:21 -0500

    refactored js, added jasmine pending tests.

---
+++ spec/javascripts/cat_form_spec.js
@@ -0,0 +1,16 @@
+describe("Cat form page", function() {
+
+  xit("new cat pages will show 40 character remaining", function() {
+    // code $(document).ready() isn't testable
+    // tested with Capybara, features/cat_form_spec.js
+  });
+
+  xit("shows characters remaining in edit cat page", function() {
+    // tested with Capybara, features/cat_form_spec.js
+  });
+
+  xit("will update remaining characters as you type", function() {
+    // tested with Capybara, features/cat_form_spec.js
+  });
+
+});
